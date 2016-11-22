2cb66f886578731bcd8084d85f27da46324695ab
Syedbakhtiyar <Syedbakhtiyar120@gmail.com>
2016-11-23 01:12:54 +0500

    quizz

---
+++ quiz/src/MyClass.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Syed Inkisar Ahmed
+ */
+public class MyClass {
+    public static int score;
+
+
+
+    public int getScore() {
+        return score;
+    }
+    
+    
+    
+    
+    
+    
+}
